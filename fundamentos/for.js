bce5179f8888f299fba8d26b05ca309db5139ed3
amanda-nogueira <amanda.alves.n@hotmail.com>
2024-08-12 19:08:30 -0300

    exercicios

---
+++ fundamentos/for.js
@@ -0,0 +1,13 @@
+/**
+ *  Exemplo de uso da estrutura FOR
+ * @author Amanda
+ */
+console.clear()
+for(let i = 1; i < 11; i++){
+    console.log(i)
+}
+console.log('')
+for(let i = 10; i > 0; i--){
+    console.log(i)
+}
+console.log('')
